5fa713090f2a46ddc85f03bb579d775d45ba00b9
Maged Mohammed <maged.mo.it@gmail.com>
2024-10-23 21:54:18 +0300

    Initial Commit

---
+++ MarvelWithSwiftUI/MarvelWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelWithSwiftUIApp.swift
+//  MarvelWithSwiftUI
+//
+//  Created by Maged on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarvelWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
